a9f4520d5f16addecc96d2ecc8b10bee291bd833
Devonarya <devonaryadaniswra@gmail.com>
2025-04-10 11:16:16 +0700

    menambahkan class DataMahasiswa103022300085

---
+++ modul7_kelompok_7/DataMahasiswa103022300085.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_7
+{
+    public class Nama
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+    public class address
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+    public class DataMahasiswa103022300085
+    {
+        public Nama nama { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public List<address> alamat { get; set; }
+        public List<Course> courses { get; set; }
+
+
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("jurnal7_1_103022300085.json");
+            DataMahasiswa103022300085 data = JsonSerializer.Deserialize<DataMahasiswa103022300085>(jsonString);
+
+            Console.WriteLine($"Data {data.nama.firstName} {data.nama.lastName} : {data.gender}, {data.age}, Alamat : {data.alamat}, {data.alamat.city}{data.alamat.state}");
+            Console.WriteLine("Daftar mata kuliah yang diambil:");
+            int i = 1;
+            foreach (var course in data.courses)
+            {
+                Console.WriteLine($"MK {i} {course.code} - {course.name}");
+                i++;
+            }
+
+        }
+    }
+}
--- modul7_kelompok_7/Program.cs
+++ modul7_kelompok_7/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul7_kelompok_7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa103022330152 mhs = new DataMahasiswa103022330152();
+        mhs.ReadJSON();
+    }
+}
